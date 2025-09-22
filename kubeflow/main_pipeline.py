81e67bdd7ec892d62a8b022e36d3eefeb07b73a6
Marco Caimi <mcaimi@redhat.com>
2025-09-22 17:27:18 +0200

    Add scheduling hints for GPU-enabled nodes

--- kubeflow/main_pipeline.py
+++ kubeflow/main_pipeline.py
@@ -16,111 +16,127 @@ from save_model import push_to_model_registry
 # Pipeline definition
 
 # name of the data connection that points to the s3 model storage bucket
-artifacts_connection_secret_name = 's3-artifacts'
-data_connection_secret_name = 's3-models'
-kaggle_api_secrets = 'yolo-kaggle'
-huggingface_api_secret = 'huggingface-secret'
+artifacts_connection_secret_name = "s3-artifacts"
+data_connection_secret_name = "s3-models"
+kaggle_api_secrets = "yolo-kaggle"
+huggingface_api_secret = "huggingface-secret"
 
 
 # Create pipeline
 @pipeline(
-  name='yolo-custom-training-pipeline',
-  description='Dense Neural Network CNN Image Detector based on YOLO'
+    name="yolo-custom-training-pipeline",
+    description="Dense Neural Network CNN Image Detector based on YOLO",
 )
-def training_pipeline(hyperparameters: dict,
-                      model_name: str,
-                      dataset_name: str,
-                      version: str,
-                      s3_deployment_name: str,
-                      s3_region: str,
-                      author_name: str,
-                      cluster_domain: str,
-                      huggingface_repo: str,
-                      data_mount_path: str,
-                      prod_flag: bool):
-
+def training_pipeline(
+    hyperparameters: dict,
+    model_name: str,
+    dataset_name: str,
+    version: str,
+    s3_deployment_name: str,
+    s3_region: str,
+    author_name: str,
+    cluster_domain: str,
+    huggingface_repo: str,
+    data_mount_path: str,
+    prod_flag: bool,
+):
     # Fetch Data from Kaggle
     fetch_dataset_task = fetch_data(dataset_name=dataset_name, version=version)
     kubernetes.use_secret_as_env(
         fetch_dataset_task,
         secret_name=huggingface_api_secret,
         secret_key_to_env={
-            'HF_HOME': 'HF_HOME',
-            'HF_TOKEN': 'HF_TOKEN',
+            "HF_HOME": "HF_HOME",
+            "HF_TOKEN": "HF_TOKEN",
         },
     )
 
     # download base model from HF
-    fetch_model_task = fetch_model(model_name=huggingface_repo,
-                                   version=version,
-                                   hyperparameters=hyperparameters)
+    fetch_model_task = fetch_model(
+        model_name=huggingface_repo, version=version, hyperparameters=hyperparameters
+    )
     kubernetes.use_secret_as_env(
         fetch_model_task,
         secret_name=kaggle_api_secrets,
         secret_key_to_env={
-            'KAGGLE_USERNAME': 'KAGGLE_USERNAME',
-            'KAGGLE_KEY': 'KAGGLE_KEY',
-            'KAGGLEHUB_CACHE': 'KAGGLEHUB_CACHE',
+            "KAGGLE_USERNAME": "KAGGLE_USERNAME",
+            "KAGGLE_KEY": "KAGGLE_KEY",
+            "KAGGLEHUB_CACHE": "KAGGLEHUB_CACHE",
         },
     )
 
     # prepare data on persistent storage for training
-    #data_disk = kubernetes.CreatePVC(
+    # data_disk = kubernetes.CreatePVC(
     #    pvc_name_suffix="-training-pvc",
     #    access_modes=['ReadWriteOnce'],
     #    size='50Gi',
     #    storage_class_name='ocs-storagecluster-ceph-rbd',
-    #)
+    # )
 
     # unzip dataset to persistent storage
-    unzip_dataset_task = unzip_dataset(data_dir=data_mount_path,
-                                       dataset=fetch_dataset_task.outputs["dataset"])
+    unzip_dataset_task = unzip_dataset(
+        data_dir=data_mount_path, dataset=fetch_dataset_task.outputs["dataset"]
+    )
     unzip_dataset_task.after(fetch_dataset_task)
 
     # mount persistent volume...
     kubernetes.mount_pvc(
         unzip_dataset_task,
-        pvc_name='training', #data_disk.outputs['name'],
+        pvc_name="training",  # data_disk.outputs['name'],
         mount_path="/data",
     )
 
     # unzip model to persistent storage
-    unzip_model_task = unzip_model(data_dir=data_mount_path,
-                                   model=fetch_model_task.outputs["original_model"])
+    unzip_model_task = unzip_model(
+        data_dir=data_mount_path, model=fetch_model_task.outputs["original_model"]
+    )
     unzip_model_task.after(unzip_dataset_task)
 
     # mount persistent volume...
     kubernetes.mount_pvc(
         unzip_model_task,
-        pvc_name='training',
+        pvc_name="training",
         mount_path="/data",
     )
 
     # train model
-    train_model_task = train_model(original_model=fetch_model_task.outputs["original_model"],
-                                   dataset=fetch_dataset_task.outputs["dataset"],
-                                   hyperparameters=hyperparameters,
-                                   data_mount_path=data_mount_path)
+    train_model_task = train_model(
+        original_model=fetch_model_task.outputs["original_model"],
+        dataset=fetch_dataset_task.outputs["dataset"],
+        hyperparameters=hyperparameters,
+        data_mount_path=data_mount_path,
+    )
     train_model_task.set_cpu_limit("8")
     train_model_task.set_memory_limit("24G")
+    # comment this to use a gpu
+    # train_model_task.set_accelerator_type(°nvidia.com/gpu").set_accelerator_limit("1")
+    # kubernetes.add_tolerations(
+    #   train_model_task,
+    #   key=°nvidia.com/gpu°,
+    #   operator='Equals',
+    #   value="True",
+    #   effect="NoSchedule"
+    # )
     # mount persistent volume...
     kubernetes.mount_pvc(
         train_model_task,
-        pvc_name='training',
+        pvc_name="training",
         mount_path="/data",
     )
     train_model_task.after(unzip_model_task)
     train_model_task.after(unzip_dataset_task)
 
     # convert to onnx
-    convert_onnx_task = convert_model(data_dir=data_mount_path,
-                                      finetuned_model=train_model_task.outputs["finetuned_model"])
+    convert_onnx_task = convert_model(
+        data_dir=data_mount_path,
+        finetuned_model=train_model_task.outputs["finetuned_model"],
+    )
     convert_onnx_task.set_cpu_limit("8")
     convert_onnx_task.set_memory_limit("24G")
     # mount persistent volume...
     kubernetes.mount_pvc(
         convert_onnx_task,
-        pvc_name='training',
+        pvc_name="training",
         mount_path="/data",
     )
     convert_onnx_task.after(train_model_task)
@@ -139,29 +155,29 @@ def training_pipeline(hyperparameters: dict,
         model_registry_task,
         secret_name=data_connection_secret_name,
         secret_key_to_env={
-            'AWS_S3_ENDPOINT': 'AWS_S3_ENDPOINT',
-            'AWS_ACCESS_KEY_ID': 'AWS_ACCESS_KEY_ID',
-            'AWS_SECRET_ACCESS_KEY': 'AWS_SECRET_ACCESS_KEY',
-            'AWS_S3_BUCKET': 'AWS_S3_BUCKET',
-            'AWS_DEFAULT_REGION': 'AWS_DEFAULT_REGION',
+            "AWS_S3_ENDPOINT": "AWS_S3_ENDPOINT",
+            "AWS_ACCESS_KEY_ID": "AWS_ACCESS_KEY_ID",
+            "AWS_SECRET_ACCESS_KEY": "AWS_SECRET_ACCESS_KEY",
+            "AWS_S3_BUCKET": "AWS_S3_BUCKET",
+            "AWS_DEFAULT_REGION": "AWS_DEFAULT_REGION",
         },
     )
     # mount persistent volume...
     kubernetes.mount_pvc(
         model_registry_task,
-        pvc_name='training',
+        pvc_name="training",
         mount_path="/data",
     )
     model_registry_task.after(convert_onnx_task)
 
     # remove pvc after pipeline completes
-    #delete_datadisk = kubernetes.DeletePVC(
+    # delete_datadisk = kubernetes.DeletePVC(
     #    pvc_name=data_disk.outputs['name']
-    #).after(unzip_model_task)
+    # ).after(unzip_model_task)
 
 
 # start pipeline
-if __name__ == '__main__':
+if __name__ == "__main__":
     metadata = {
         "hyperparameters": {
             "epochs": 1,
@@ -185,39 +201,36 @@ if __name__ == '__main__':
         "cluster_domain": "apps.prod.rhoai.rh-aiservices-bu.com",
         "huggingface_repo": "Ultralytics/YOLO11",
         "data_mount_path": "/data",
-        "prod_flag": False
+        "prod_flag": False,
     }
 
-    namespace_file_path =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-    with open(namespace_file_path, 'r') as namespace_file:
+    namespace_file_path = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+    with open(namespace_file_path, "r") as namespace_file:
         namespace = namespace_file.read()
 
-    kubeflow_endpoint =\
-        f'https://ds-pipeline-dspa.{namespace}.svc:8443'
+    kubeflow_endpoint = f"https://ds-pipeline-dspa.{namespace}.svc:8443"
 
-    sa_token_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/token'
-    with open(sa_token_file_path, 'r') as token_file:
+    sa_token_file_path = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+    with open(sa_token_file_path, "r") as token_file:
         bearer_token = token_file.read()
 
-    ssl_ca_cert =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt'
+    ssl_ca_cert = "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
 
     # compile pipeline for debugging
     from kfp import compiler
-    compiler.Compiler().compile(training_pipeline, 'pipeline.yaml')
+
+    compiler.Compiler().compile(training_pipeline, "pipeline.yaml")
 
     # Run pipeline on cluster
-    print(f'Connecting to Data Science Pipelines: {kubeflow_endpoint}')
+    print(f"Connecting to Data Science Pipelines: {kubeflow_endpoint}")
     client = kfp.Client(
-        host=kubeflow_endpoint,
-        existing_token=bearer_token,
-        ssl_ca_cert=ssl_ca_cert
+        host=kubeflow_endpoint, existing_token=bearer_token, ssl_ca_cert=ssl_ca_cert
     )
 
     client.create_run_from_pipeline_func(
         training_pipeline,
         arguments=metadata,
         experiment_name="yolo-custom-training-pipeline",
-        enable_caching=True
+        enable_caching=True,
     )
+
